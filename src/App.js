db91cf393f1adea27b1ce27db92714023ed46def
Sebastian Stec <sebastianstec@Sebastians-MBP.lan>
2022-01-13 14:43:58 -0500

    hourly commit, forgot to commit files yesterday

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import LogIn from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'Sebastian_S',
+        memberSince: '08/23/99',
+      }
+    }
+  }
+  LogIn = (Info) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = Info.userName
+    this.setState({currentUser: newUser})
+  }
+
+  render() {
+
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>);
+   const LogInComponent = () => (<LogIn user={this.state.currentUser} LogIn={this.LogIn} {...this.props}/>);
+
+    return (
+        <Router>
+          <div>
+            <Routes>
+            <Route exact path="/UserProfile" element={<UserProfileComponent/>}/>
+            <Route exact path="/LogIn" element={<LogInComponent/>}/>
+            <Route exact path="/" element={<HomeComponent/>}/>
+            </Routes>
+          </div>
+        </Router>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/debit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App(){
+    const [amount, setAmount] = useState(0);
+    const [name, setName] = useState("");
+    const [debits, setDebit] = useState([
+        { amount: 200, name: "Supplies" },
+        { amount: 100, name: "Rent" },
+        { amount: 75, name: "Car Expenses" },
+    ]);
+
+    
+}
